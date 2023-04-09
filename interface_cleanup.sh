7f567501e9a2c81b4f57adb7f0bb90f4c483ef81
evanstella <evan.p.stella@gmail.com>
2023-04-09 18:41:55 -0400

    updates to demo infrastructure

--- interface_cleanup.sh
+++ interface_cleanup.sh
@@ -2,3 +2,8 @@ sudo ip link set virbr0 down
 
 sudo ip tuntap del dev tap0 mode tap
 sudo brctl delbr virbr0
+
+sudo iptables -F
+sudo iptables -t nat -F
+
+sudo iptables -t nat -L
--- interface_setup.sh
+++ interface_setup.sh
@@ -26,6 +26,11 @@ sudo iptables -A FORWARD -i virbr0 -o wlo1 -j ACCEPT
 # ...and the other way
 sudo iptables -A FORWARD -i wlo1 -o virbr0 -j ACCEPT
 
+# NAT all traffic into port 10000 to composite
+sudo iptables -t nat -A PREROUTING -p udp --dport 10000 -j DNAT --to-destination 10.10.1.2:8888
+
 # NAT all traffic out of wlo1
-sudo iptables -t nat -A POSTROUTING -o wlo1 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+
+sudo iptables -t nat -L
 
